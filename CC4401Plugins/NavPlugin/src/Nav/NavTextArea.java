3c44522f34b4e71ea701a65b85a6b2e9b58a1e56
luism <luis.martinezqug.uchile.cl>
2014-10-20 17:53:14 -0300

    Agregada capacidad para navegar por el panel y documentacion

--- CC4401Plugins/NavPlugin/src/Nav/NavTextArea.java
+++ CC4401Plugins/NavPlugin/src/Nav/NavTextArea.java
@@ -31,44 +31,43 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseMotionListener;
-import java.awt.font.TextAttribute;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.swing.JScrollBar;
-import javax.swing.event.MouseInputAdapter;
 
-import org.gjt.sp.jedit.*;
-import org.gjt.sp.jedit.msg.BufferUpdate;
-import org.gjt.sp.jedit.msg.EditPaneUpdate;
-import org.gjt.sp.jedit.msg.PropertiesChanged;
 import org.gjt.sp.jedit.syntax.SyntaxStyle;
 import org.gjt.sp.jedit.textarea.AntiAlias;
-import org.gjt.sp.jedit.textarea.DisplayManager;
 import org.gjt.sp.jedit.textarea.JEditEmbeddedTextArea;
 import org.gjt.sp.jedit.textarea.JEditTextArea;
-import org.gjt.sp.jedit.textarea.ScrollListener;
-import org.gjt.sp.jedit.textarea.TextArea;
 import org.gjt.sp.jedit.textarea.TextAreaPainter;
-import org.gjt.sp.util.Log;
 
 /**
  * Created by luism on 18-10-14.
+ */
+
+/**
+ * NavTextArea is the class that represents the textArea, where the visualization is embedded
  */
 public class NavTextArea extends JEditEmbeddedTextArea
 {
 	private JScrollBar scrollBar;
 	private JEditTextArea textArea;
+	private MouseMotionListener mouseMotionListener;
+	private MouseListener mouseListener;
+	private int DragStarLineJEdit;
+	private int DragStarLineText;
+	private Point dragStart;
+	/**
+	 * Constructor of the class
+	 * @param textArea The panel to which it is associated the plugin.
+	 */
 	public NavTextArea(JEditTextArea textArea){
-		try{
-			System.setOut(new PrintStream(new FileOutputStream("salida_normal.txt")));
-		}catch (IOException e){
-			throw new RuntimeException("Error Fatal");
-		}
 		this.textArea = textArea;
+		mouseMotionListener=new NavMouseMotionListener();
+		mouseListener=new NavMouseListener();
+		scrollBar=getScrollBar(this);
+		scrollBar.setVisible(false);
+		painter.addMouseMotionListener(mouseMotionListener);
+		painter.addMouseListener(mouseListener);
 		getBuffer().setProperty("folding", "explicit");
 		setBuffer(textArea.getBuffer());
 		getPainter().setCursor(
@@ -92,6 +91,10 @@ public class NavTextArea extends JEditEmbeddedTextArea
 			}
 		});
 	}
+
+	/**
+	 * Resize the font in the textAre
+	 */
 	private void resizeFont(){
 		TextAreaPainter painter = getPainter();
 		Font f = deriveFont(painter.getFont());
@@ -99,15 +102,153 @@ public class NavTextArea extends JEditEmbeddedTextArea
 		SyntaxStyle [] styles = painter.getStyles();
 		updateStyles(styles);
 	}
+
+	/**
+	 * Takes a Font and returns de new font (smaller)
+	 * @param f the font of the JeditTextArea
+	 * @return The new font for the text Area
+	 */
 	private static Font deriveFont(Font f) {
-		return new Font("Verdana", Font.BOLD, 9);
+		return new Font("Verdana", Font.PLAIN, 7);
 	}
+
+	/**
+	 * Update the style of the textArea
+	 * @param styles, syntax Style of the JEditTextArea
+	 */
 	private static void updateStyles(SyntaxStyle[] styles) {
 		for (int i = 0; i < styles.length; i++) {
 			SyntaxStyle style = styles[i];
 			styles[i] = new SyntaxStyle(style.getForegroundColor(),
 						    style.getBackgroundColor(),
 						    deriveFont(style.getFont()));
+		}
+	}
+
+	/**
+	 *
+	 * @param container, the place where the scrollBar is search
+	 * @return the scrollBar of the textArea if exists
+	 */
+	private JScrollBar getScrollBar(Container container)
+	{
+		for (Component comp: container.getComponents()) {
+			if (comp instanceof JScrollBar)
+				return (JScrollBar) comp;
+			if (comp instanceof Container) {
+				JScrollBar sb = getScrollBar((Container) comp);
+				if (sb != null)
+					return sb;
+			}
+		}
+		return null;
+	}
+	@Override
+	/**
+	 * Draws a red rectangle in the textArea, the size of the rectangle is the same of JeditTextArea
+	 */
+	public void paint(Graphics graphics){
+		super.paint(graphics);
+		//Cantidad de lineas visibles en textArea
+		int width = painter.getWidth() - 1;
+		int h = painter.getFontMetrics().getHeight();
+		int firstPhysicalLine = textArea.getFirstPhysicalLine();
+		int height = textArea.getVisibleLines() * h - 1;
+		graphics.setColor(Color.RED);
+		graphics.drawRect(0,0,width,height);//Hay que arreglar esto!! el segundo valor no tiene que ser 0
+	}
+
+	/**
+	 *
+	 * @param line, the new physical line of the textArea
+	 */
+	private void scrollTextArea(int line){
+		int visibleLines = textArea.getVisibleLines();
+		int count = textArea.getLineCount();
+		if (line > count - visibleLines)
+			line = count - visibleLines;
+		if (line < 0)
+			line = 0;
+		textArea.setFirstPhysicalLine(line);
+		int first = getFirstPhysicalLine();
+		if (line < first)
+			setFirstPhysicalLine(line);
+		else if (line + visibleLines > getLastPhysicalLine())
+			setFirstPhysicalLine(line + visibleLines - getVisibleLines());
+		repaint();
+
+	}
+	private class NavMouseMotionListener extends MouseMotionAdapter
+	{
+		@Override
+		public void mouseDragged(MouseEvent mouseEvent)
+		{
+			if (dragStart == null)
+				return;
+			TextAreaPainter painter = getPainter();
+			int h = painter.getFontMetrics().getHeight();
+			int amount = (int) (mouseEvent.getY() - dragStart.getY()) / h;
+			int line = DragStarLineJEdit + getFirstPhysicalLine() - DragStarLineText;
+			if (amount < 0)
+			{
+				for (int i = 0;i<-amount;i++)
+				{
+					int prevLine = getDisplayManager().getPrevVisibleLine(line);
+					if (prevLine != -1)
+						line = prevLine;
+					else
+						break;
+				}
+			}
+			else if (amount > 0)
+			{
+				for (int i = 0;i<amount;i++)
+				{
+					int nextLine = getDisplayManager().getNextVisibleLine(line);
+					if (nextLine != -1)
+						line = nextLine;
+					else
+						break;
+				}
+			}
+			scrollTextArea(line);
+			mouseEvent.consume();
+		}
+	}
+	private class NavMouseListener extends MouseAdapter
+	{
+		@Override
+		public void mousePressed(MouseEvent e){
+			if (e.getButton() != MouseEvent.BUTTON1)
+				return;
+			int offset = xyToOffset(0, e.getY());
+			int line = getLineOfOffset(offset);
+			if (line > textArea.getLastPhysicalLine() || line < textArea.getFirstPhysicalLine())
+			{
+				int visibleLines = textArea.getVisibleLines();
+
+				int back = visibleLines >> 1;
+				for (int i =0;i<back;i++)
+				{
+					line = getDisplayManager().getPrevVisibleLine(line);
+					if (line == 0)
+						break;
+				}
+				scrollTextArea(line);
+			}
+			dragStart = e.getPoint();
+			DragStarLineJEdit = textArea.getFirstPhysicalLine();
+			DragStarLineText = getFirstPhysicalLine();
+			e.consume();
+		}
+		@Override
+		public void mouseReleased(MouseEvent e){
+			if (e.getButton() != MouseEvent.BUTTON1)
+				return;
+			if (dragStart != null)
+				e.consume();
+
+			dragStart = null;
 		}
 	}
 }
