b2779bfc00c0a0e5912a29f96d7771e2a5c7cced
segonzal <seba-5@hotmail.com>
2014-11-18 11:23:25 -0300

    Test: arroja la url del archivo a la salida de errores

--- CC4401plugins/HtmlViewer/htmlviewer/Test.java
+++ CC4401plugins/HtmlViewer/htmlviewer/Test.java
@@ -1,10 +1,13 @@
 package htmlviewer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Test{
-    
     public static final void launchBrowser(String url){
-        System.out.println(url);
+        System.err.println(url);
     }
-    
     
 }
