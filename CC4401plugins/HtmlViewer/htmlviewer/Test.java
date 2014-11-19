f4651cb4c0df2560e92884db96d9948a31687b9d
segonzal <seba-5@hotmail.com>
2014-11-19 16:23:09 -0300

    Agregando Display. PRECAUCION: NO FUNCIONA

--- CC4401plugins/HtmlViewer/htmlviewer/Test.java
+++ CC4401plugins/HtmlViewer/htmlviewer/Test.java
@@ -1,13 +1,34 @@
 package htmlviewer;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import org.eclipse.swt.*;
+import org.eclipse.swt.browser.*;
+import org.eclipse.swt.custom.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+import java.io.*;
+import java.net.*;
 
 public class Test{
-    public static final void launchBrowser(String url){
-        System.err.println(url);
+    
+    public Test(){
+        System.err.println("Always look on the bright side of life!");
     }
     
+    /*
+    public Test(String st){
+        System.err.println(st);
+    }
+    */
+    
+    public static final void launchBrowser(String url){
+        System.err.println(url);
+//        Display display = new Display();
+//      final Shell shell = new Shell(display);
+//      shell.setText("SWT Browser - Documentation Viewer");
+//      shell.setLayout(new GridLayout());
+//      Composite compTools = new Composite(shell, SWT.NONE);
+//	    GridData data = new GridData(GridData.FILL_HORIZONTAL);
+//	    compTools.setLayoutData(data);
+//    	compTools.setLayout(new GridLayout(2, false));
+    }
 }
